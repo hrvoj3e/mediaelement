1e2a8e22d2d2908f86aac4b36870a4458b781a71
hrvoj3e <hrvoj3e@users.noreply.github.com>
2017-01-02 22:47:57 +0100

    Add Croatian language

---
+++ build/lang/hr.js
@@ -0,0 +1,135 @@
+/*!
+ * This is a i18n.locale language object for Croatian = HR.
+ *
+ * @author
+ *   Hrvoj3e (hrvoj3e@gmail.com)
+
+ * @see
+ *   mediaelement-i18n.js
+ */
+(function (exports) {
+	"use strict";
+
+	if (exports.en === undefined) {
+		exports.en = {
+			"mejs.plural-form": 1,
+
+			// me-shim
+			"mejs.download-file": "Preuzmi datoteku",
+			"mejs.install-flash": "Koristote preglednik koji nema omogućen ili instaliran Flash preglednik. Molimo Vas uključite Flash dodatak ili preuzmite nojnoviju verziju s https://get.adobe.com/flashplayer/",
+
+			// mediaelementplayer-feature-contextmenu
+			"mejs.fullscreen-off": "Isključi puni zaslon",
+			"mejs.fullscreen-on": "Uključi puni zaslon",
+			"mejs.download-video": "Preuzmi video",
+
+			// mediaelementplayer-feature-fullscreen
+			"mejs.fullscreen": "Puni zaslon",
+
+			// mediaelementplayer-feature-jumpforward
+			"mejs.time-jump-forward": ["Skoči naprijed 1 sekundu", "Skoči naprijed %1 sekundi"],
+
+			// mediaelementplayer-feature-loop
+			"mejs.loop": "Uključi/isključi ponavljanje",
+
+			// mediaelementplayer-feature-playpause
+			"mejs.play": "Pokreni",
+			"mejs.pause": "Zaustavi",
+
+			// mediaelementplayer-feature-postroll
+			"mejs.close": "Zatvori",
+
+			// mediaelementplayer-feature-progress
+			"mejs.time-slider": "Vremenska traka",
+			"mejs.time-help-text": "Koristi strelice lijevo/desno za pomak naprijed za 1 sekundu te gore/dolje za pomak od 10 sekundi.",
+
+			// mediaelementplayer-feature-skipback
+			"mejs.time-skip-back": ["Skoči natrag 1 sekundu", "Skoči natrag %1 sekundi"],
+
+			// mediaelementplayer-feature-tracks
+			"mejs.captions-subtitles": "Opisi/Prijevodi",
+			"mejs.none": "Ništa",
+
+			// mediaelementplayer-feature-volume
+			"mejs.mute-toggle": "Uključi/isključi zvuk",
+			"mejs.volume-help-text": "Koristi strelice gore/dolje za pojačavanje ili stišavanje.",
+			"mejs.unmute": "Uključi zvuk",
+			"mejs.mute": "Isključi zvuk",
+			"mejs.volume-slider": "Pokazivač razine zvuka",
+
+			// mep-player
+			"mejs.video-player": "Video reprodukcija",
+			"mejs.audio-player": "Audio reprodukcija",
+
+			// mediaelementplayer-feature-ads
+			"mejs.ad-skip": "Preskoči oglas",
+			"mejs.ad-skip-info": ["Preskoči za 1 sekundu", "Preskoči za %1 sekundi"],
+
+			// mediaelementplayer-feature-sourcechooser
+			"mejs.source-chooser": "Source Chooser",
+
+			// mediaelementplayer-feature-stop
+			"mejs.stop": "Zaustavi",
+
+			//mediaelementplayer-feature-speed
+			"mejs.speed-rate" : "Speed Rate",
+
+			// mep-tracks
+			"mejs.afrikaans": "Afrički",
+			"mejs.albanian": "Albanski",
+			"mejs.arabic": "Arapski",
+			"mejs.belarusian": "Bjeloruski",
+			"mejs.bulgarian": "Bugarski",
+			"mejs.catalan": "Katalonski",
+			"mejs.chinese": "Kineski",
+			"mejs.chinese-simplified": "Kineski (jednostavni)",
+			"mejs.chinese-traditional": "Kineski (tradicionalni)",
+			"mejs.croatian": "Hrvatski",
+			"mejs.czech": "Češki",
+			"mejs.danish": "Danski",
+			"mejs.dutch": "Nizozemski",
+			"mejs.english": "Engleski",
+			"mejs.estonian": "Estonski",
+			"mejs.filipino": "Filipinski",
+			"mejs.finnish": "Finski",
+			//"mejs.french": "Francuski",
+			//"mejs.galician": "Galician",
+			"mejs.german": "Njemački",
+			"mejs.greek": "Grčki",
+			//"mejs.haitian-creole": "Haitian Creole",
+			//"mejs.hebrew": "Hebrew",
+			"mejs.hindi": "Hindi",
+			"mejs.hungarian": "Mađarski",
+			"mejs.icelandic": "Islandski",
+			"mejs.indonesian": "Indonezijski",
+			"mejs.irish": "Irski",
+			"mejs.italian": "Talijanski",
+			"mejs.japanese": "Japanski",
+			"mejs.korean": "Korejski",
+			"mejs.latvian": "Latvijski",
+			"mejs.lithuanian": "Litvanski",
+			"mejs.macedonian": "Makedonski",
+			//"mejs.malay": "Malay",
+			//"mejs.maltese": "Maltese",
+			"mejs.norwegian": "Norveški",
+			"mejs.persian": "Perzijski",
+			"mejs.polish": "Poljski",
+			"mejs.portuguese": "Portugalski",
+			"mejs.romanian": "Rumunjski",
+			"mejs.russian": "Ruski",
+			"mejs.serbian": "Srpski",
+			"mejs.slovak": "Slovački",
+			"mejs.slovenian": "Slovenski",
+			"mejs.spanish": "Španjolski",
+			"mejs.swahili": "Svahili",
+			"mejs.swedish": "Švedski",
+			//"mejs.tagalog": "Tagalog",
+			//"mejs.thai": "Thai",
+			"mejs.turkish": "Turski",
+			"mejs.ukrainian": "Ukrajinski",
+			//"mejs.vietnamese": "Vietnamese",
+			//"mejs.welsh": "Welsh",
+			//"mejs.yiddish": "Yiddish"
+		};
+	}
+}(mejs.i18n.locale.strings));
